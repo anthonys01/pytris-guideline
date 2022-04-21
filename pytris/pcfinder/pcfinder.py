bd9eca2abd5bb3ee9df280c9eba6c4c461844a53
Anthonys01 <anthonys01.levrai@gmail.com>
2022-04-21 21:28:46 +0200

    first try at splitting

--- pytris/pcfinder/pcfinder.py
+++ pytris/pcfinder/pcfinder.py
@@ -1,7 +1,10 @@
 """
     Find PC sequences for given queue and board
 """
+import itertools
 from typing import List, Tuple, Optional, Iterable
+
+import more_itertools
 
 from pytris.pieces import T_PIECE, Z_PIECE, L_PIECE, J_PIECE, S_PIECE, I_PIECE, O_PIECE, PIECES_ROT, reverse_rotate
 from pytris.typehints import BoolGrid, PiecePos
@@ -57,6 +60,17 @@ class PCFinder:
             working_nodes = res + working_nodes
 
         return to_return
+
+    def generate_sub_queue(self, queue: Queue, sub_size: int, max_size: int)\
+            -> Iterable[Tuple[Queue, List[int], Queue]]:
+        reduced = queue[:max_size]
+        sub_filter = [0] * max_size
+        for i in range(sub_size):
+            sub_filter[i] = 1
+        for perm in more_itertools.distinct_permutations(sub_filter):
+            sub_res = list(itertools.compress(reduced, perm))
+            left_queue = [reduced[i] for i in range(max_size) if perm[i] == 0]
+            yield sub_res, perm, left_queue
 
     @staticmethod
     def get_column_parity(grid_state: BoolGrid) -> int:
@@ -136,13 +150,23 @@ class PCFinder:
 
         return new_grid_state
 
-    def grids_after_placing(self, piece: int, grid_state: BoolGrid) \
+    def grids_after_placing(self, piece: int, grid_state: BoolGrid,
+                            column_parity: int, ignore_left: int, ignore_right: int) \
             -> Iterable[Tuple[int, PiecePos, BoolGrid]]:
         rot_pos = PIECES_ROT[piece]
 
+        authorized_rot = [0, 1, 2, 3]
+        if piece == T_PIECE:
+            if column_parity == 0:
+                authorized_rot = [0, 2]
+            elif column_parity == 1:
+                authorized_rot = [1, 3]
+
         for rot, piece_rot_pos in enumerate(rot_pos):
+            if rot not in authorized_rot:
+                continue
             for line_index in reversed(range(-1, len(grid_state))):
-                for col_index in range(-1, len(grid_state[line_index])):
+                for col_index in range(-1 + ignore_left, len(grid_state[line_index]) - ignore_right):
                     translated_pos: PiecePos = []
                     in_the_box = True
                     for pos in piece_rot_pos:
@@ -168,9 +192,25 @@ class PCFinder:
             # no solution because cannot correct parity
             return
 
+        if T_PIECE in queue[1:]:
+            # set to -1 for it to be ignored
+            current_parity = -1
+
         tested_pos = []
 
-        for piece_rot, piece_pos, possible_grid_state in self.grids_after_placing(queue[0], grid_state):
+        ignore_left, ignore_right = 0, 0
+        for col in range(len(grid_state[0]) // 2):
+            if sum(line[col] for line in grid_state) < len(grid_state):
+                break
+            ignore_left += 1
+        for col in reversed(range(len(grid_state[0]))):
+            if sum(line[col] for line in grid_state) < len(grid_state):
+                break
+            ignore_right += 1
+
+        for piece_rot, piece_pos, possible_grid_state in self.grids_after_placing(queue[0], grid_state,
+                                                                                  current_parity,
+                                                                                  ignore_left, ignore_right):
             res = self.solve_for(queue[1:],
                                  possible_grid_state,
                                  movements + [[piece_pos]])
@@ -180,12 +220,19 @@ class PCFinder:
                     res[res.index([piece_pos])] = mov
                     return res
 
+    def get_left_queue(self, full_queue: Queue, used_queue: Queue) -> Queue:
+        queue = full_queue[:]
+        for piece in used_queue:
+            queue.remove(piece)
+        return queue
+
     def solve(self, queue: Queue, grid_state: BoolGrid) -> Optional[Tuple[Queue, List[PieceMov]]]:
         """
             Solve for given queue and grid state
         """
+        line_nb, col_nb = len(grid_state), len(grid_state[0])
         filled_cell = sum(sum(line) for line in grid_state)
-        unused_cells = len(grid_state) * len(grid_state[0]) - filled_cell
+        unused_cells = line_nb * col_nb - filled_cell
         if unused_cells % 4 != 0:
             print("invalid grid")
             return
@@ -193,10 +240,37 @@ class PCFinder:
         if len(queue) < needed_pieces:
             print(f"invalid queue length : need at least {needed_pieces} but we have {len(queue)}")
             return
-        for gen_queue in self.generate_possible_queue_combinations(queue, needed_pieces):
-            res = self.solve_for(gen_queue, grid_state, [])
-            if res:
-                return gen_queue, res
+
+        possible_splits = []
+        current_unused = 0
+        for col in range(col_nb):
+            current_unused += line_nb - sum(line[col] for line in grid_state)
+            if current_unused % 4 == 0:
+                possible_splits.append(col)
+        print(f"Found possible splits : {possible_splits}")
+
+        for split in possible_splits:
+            print(f"Trying split {split}")
+            sub_grid = [line[:split + 1] for line in grid_state]
+            filled = sum(sum(line) for line in sub_grid)
+            unused = line_nb * (split + 1) - filled
+            sub_needed = unused // 4
+            for gen_queue in self.generate_possible_queue_combinations(queue, sub_needed):
+                res = self.solve_for(gen_queue, sub_grid, [])
+                if res:
+                    print("Found sub-solve")
+                    if split + 1 == col_nb:
+                        return gen_queue, res
+
+                    left = self.get_left_queue(queue, gen_queue)
+                    left_grid = [line[:] for line in grid_state]
+                    for line in left_grid:
+                        for col in range(split + 1):
+                            line[col] = True
+                    for gen_queue2 in self.generate_possible_queue_combinations(left, needed_pieces - sub_needed):
+                        res2 = self.solve_for(gen_queue2, left_grid, [])
+                        if res2:
+                            return gen_queue + gen_queue2, res + res2
 
 
 if __name__ == "__main__":
